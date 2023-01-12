b3018730d21f5b3d9338a6f2f00ea9c8cd579786
Sankalpa75 <bhattaraisankalpa1@gmail.com>
2023-01-12 21:30:06 +0545

    messi

---
+++ html/includes/banner.php
@@ -0,0 +1,72 @@
+
+<div id="owl-demo" class="owl-carousel owl-theme">
+
+  <div class="item">
+    <div class="banner-content">
+      <img src="images/b1.jpg">
+      
+    </div>
+  </div>
+
+   <div class="item">
+    <div class="banner-content">
+      <img src="images/b2.jpg">
+      
+    </div>
+  </div>
+
+     <div class="item">
+    <div class="banner-content">
+      <img src="images/b3.jpg">
+      
+    </div>
+  </div>
+
+
+
+    <div class="item">
+    <div class="banner-content">
+      <img src="images/b4.jpg">
+      
+    </div>
+  </div>
+
+
+
+    <div class="item">
+    <div class="banner-content">
+      <img src="images/b5.jpg">
+      
+    </div>
+  </div>
+
+
+
+    <div class="item">
+    <div class="banner-content">
+      <img src="images/b6.jpg">
+      
+    </div>
+  </div>
+
+
+
+
+
+
+
+
+
+  
+
+
+
+  
+
+</div>
+
+
+
+
+
+
